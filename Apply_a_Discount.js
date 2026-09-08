d9c7ce8d32d6614e0da43db9816cf2dd0ebd74ab
ANASS BOURASS <anassbourass538@gmail.com>
2026-09-08 11:00:46 +0100

    challenge day 1

---
+++ Apply_a_Discount.js
@@ -0,0 +1,7 @@
+let price = 600;
+let totalprice = price - (price*0.10);
+if(price>=500){
+    console.log(totalprice);
+}else{
+    console.log(price)
+}
---
+++ Swap_Two_Values.js
@@ -0,0 +1,9 @@
+let firstColor = "red";
+let secondColor = "blue";
+
+let temp = firstColor;
+firstColor = secondColor;
+secondColor = temp;
+
+console.log(firstColor);  
+console.log(secondColor); 
