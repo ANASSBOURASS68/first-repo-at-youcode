baf44c6bedb7315a3f9a9c2b11bdefc019eb7598
ANASS BOURASS <anassbourass538@gmail.com>
2026-09-13 13:33:54 +0100

    day03 exercises

---
+++ day03/exercices/exercice-13.js
@@ -0,0 +1,32 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 03 · EXERCICE 13 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * GÉNÉRATEUR DE MOT DE PASSE FACTICE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Écrivez une fonction genererMotDePasse(longueur) qui génère et retourne une chaîne aléatoire contenant X fois la lettre "A" ou un chiffre aléatoire. (Utilisez Math.random()).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-13
+ * ▶️ Commande : node day03/exercices/exercice-13.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+// TODO: écris ta solution ici.
+function genererMotDePasse(longueur){
+    let mot = "";
+    for (let i =0 ; i<longueur ; i++ ){
+        if(Math.random() < 0.5){
+        mot= mot+"A";
+        }else{
+            let chiffre = Math.floor( Math.random()*10);
+            mot=mot+chiffre;
+        }
+    }
+    
+        return mot;
+}
+console.log(genererMotDePasse(16))
+last
